db810338c0eac8bb0543d6ac5595a01f6753a60f
Nandipha25 <nandiphabhengu78@gmail.com>
2023-03-24 09:40:28 -0700

    0x03-debugging

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return: Always 1
+ */	
+
+int main(voi)
+
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
